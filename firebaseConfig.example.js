27a41560f5bf4d4155cdc17703885ae682275839
Burak Guclu <burak.guclu@tedu.edu.tr>
2026-02-10 21:42:19 +0300

    security: .gitignore güncellendi, firebaseConfig.js gizlendi, example dosyası eklendi

---
+++ firebaseConfig.example.js
@@ -0,0 +1,12 @@
+// firebaseConfig.example.js
+// Bu dosyayı kopyalayıp "firebaseConfig.js" olarak adlandırın
+// ve kendi Firebase projenizin bilgilerini girin.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
